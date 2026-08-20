05c30459f6213e6f13d69b6945979fdb3db0edcb
smiledoneholy <smiledoneholy95@gmail.com>
2026-08-20 22:57:45 +0100

    Add 100KB page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -28,5 +28,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.5,
     },
+    {
+  url: `${baseUrl}/resize-image-to-100kb`,
+  lastModified: new Date(),
+  changeFrequency: "weekly",
+  priority: 0.9,
+},
   ];
 }
